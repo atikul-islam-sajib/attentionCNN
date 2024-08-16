77fc69f6384e5aa0e22eeb41d5c03a734762232d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-16 17:47:54 +0200

    utils file is updated

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import sys
+import yaml
 import joblib
 
 sys.path.append("./src/")
@@ -10,3 +11,8 @@ def dump(value=None, filename=None):
 
     else:
         raise ValueError("Both value and filename are required".capitalize())
+
+
+def config():
+    with open("./config.yml", "r") as file:
+        return yaml.safe_load(file)
