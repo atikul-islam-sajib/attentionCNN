e3073cd5fce56581962bfca91db8d678fe11b6dd
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-17 20:22:59 +0200

    heads is defined

--- src/multihead_attention.py
+++ src/multihead_attention.py
@@ -40,4 +40,39 @@ class MultiHeadAttentionLayer(nn.Module):
 
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
-            pass
+            QKV = self.QKV(x)
+
+            self.query, self.key, self.value = torch.chunk(input=QKV, chunks=3, dim=1)
+
+            assert (
+                self.query.size() == self.key.size() == self.value.size()
+            ), "QKV must have the same size".capitalize()
+
+            self.query = self.query.view(
+                self.query.size(0),
+                self.nheads,
+                self.channels // self.nheads,
+                self.query.size(2) * self.query.size(3),
+            )
+
+            self.key = self.key.view(
+                self.key.size(0),
+                self.nheads,
+                self.channels // self.nheads,
+                self.key.size(2) * self.key.size(3),
+            )
+
+            self.value = self.value.view(
+                self.value.size(0),
+                self.nheads,
+                self.channels // self.nheads,
+                self.value.size(2) * self.value.size(3),
+            )
+
+            return self.query
+
+
+if __name__ == "__main__":
+    attention = MultiHeadAttentionLayer(channels=128, nheads=8, bias=True)
+
+    print(attention(torch.randn(16, 128, 128, 128)).size())
