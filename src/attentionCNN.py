24a0eddba2314de93f1f7698fc84bbf5cd8a08a1
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-18 02:09:41 +0200

    bug fixed in the attentionCNN

--- src/attentionCNN.py
+++ src/attentionCNN.py
@@ -44,16 +44,6 @@ class attentionCNN(nn.Module):
             padding=self.padding_size,
             bias=self.bias,
         )
-        
-        self.encoder1 = EncoderBlock(in_channels=self.image_size, out_channels=self.image_size*2, batch_norm=True)
-        self.encoder2 = EncoderBlock(in_channels=self.image_size*2, out_channels=self.image_size*4, batch_norm=True)
-        self.encoder3 = EncoderBlock(in_channels=self.image_size*4, out_channels=self.image_size*8, batch_norm=False)
-        
-        self.decoder1 = DecoderBlock(in_channels=self.image_size*8, out_channels=self.image_size*4, batchnorm=True)
-        self.decoder2 = DecoderBlock(in_channels=self.image_size*8, out_channels=self.image_size*2, batchnorm=True)
-        self.decoder3 = DecoderBlock(in_channels=self.image_size*4, out_channels=self.image_size, batchnorm=True)
-        
-         
 
         self.attention_cnn_block = nn.Sequential(
             *[
@@ -66,6 +56,38 @@ class attentionCNN(nn.Module):
                 )
                 for _ in tqdm(range(self.num_layers))
             ]
+        )
+
+        self.encoder1 = EncoderBlock(
+            in_channels=self.image_size,
+            out_channels=self.image_size * 2,
+            batch_norm=True,
+        )
+        self.encoder2 = EncoderBlock(
+            in_channels=self.image_size * 2,
+            out_channels=self.image_size * 4,
+            batch_norm=True,
+        )
+        self.encoder3 = EncoderBlock(
+            in_channels=self.image_size * 4,
+            out_channels=self.image_size * 8,
+            batch_norm=False,
+        )
+
+        self.decoder1 = DecoderBlock(
+            in_channels=self.image_size * 8,
+            out_channels=self.image_size * 4,
+            batchnorm=True,
+        )
+        self.decoder2 = DecoderBlock(
+            in_channels=self.image_size * 8,
+            out_channels=self.image_size * 2,
+            batchnorm=True,
+        )
+        self.decoder3 = DecoderBlock(
+            in_channels=self.image_size * 4,
+            out_channels=self.image_size,
+            batchnorm=True,
         )
 
         self.output_block = nn.Conv2d(
@@ -81,19 +103,19 @@ class attentionCNN(nn.Module):
         if isinstance(x, torch.Tensor):
             x = self.input_block(x)
             x = self.attention_cnn_block(x)
-            
+
             encoder1 = self.encoder1(x)
             encoder2 = self.encoder2(encoder1)
             encoder3 = self.encoder3(encoder2)
-            
+
             decoder1 = self.decoder1(encoder3)
             decoder1 = torch.cat((decoder1, encoder2), dim=1)
-            
+
             decoder2 = self.decoder2(decoder1)
-            decoder2 = torch.cat((decoder2, encoder1), dim = 1)
-            
+            decoder2 = torch.cat((decoder2, encoder1), dim=1)
+
             decoder3 = self.decoder3(decoder2)
-            
+
             output = self.output_block(decoder3)
 
             return torch.tanh(input=output)
@@ -105,12 +127,12 @@ class attentionCNN(nn.Module):
 if __name__ == "__main__":
     attention_cnn = attentionCNN(
         image_channels=3,
-        image_size=128,
-        nheads=8,
+        image_size=256,
+        nheads=4,
         dropout=0.1,
         num_layers=1,
         activation="relu",
         bias=True,
     )
 
-    print(attention_cnn(torch.randn(16, 3, 128, 128)).size())
+    print(attention_cnn(torch.randn(1, 3, 256, 256)).size())
