13587178a8b0cae5dbf76b3eaa7e4ae272c119e5
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-18 21:29:50 +0200

    helper method is completed successfully

--- src/helper.py
+++ src/helper.py
@@ -1,34 +1,42 @@
 import os
 import sys
 import torch
-import torch.nn as nn
 import torch.optim as optim
 
 sys.path.append("./src/")
 
-from utils import config
+from utils import config, load
 from attentionCNN import attentionCNN
-from loss.dice_loss import DiceLoss
 from loss.focal_loss import FocalLoss
 from loss.jaccard_loss import IoULoss
-from loss.combo_loss import ComboLoss
+from loss.dice_loss import DiceLoss
 from loss.tversky_loss import TverskyLoss
 from loss.bce_loss import BinaryCrossEntropyLoss
 
+
 def load_dataloader():
     if os.path.exists(config()["path"]["PROCESSED_PATH"]):
-        train_dataloader = os.path.join(config()["path"]["PROCESSED_PATH"], "train_dataloader.pkl")
-        valid_dataloader = os.path.join(config()["path"]["PROCESSED_PATH"], "valid_dataloader.pkl")
-        test_dataloader = os.path.join(config()["path"]["PROCESSED_PATH"], "test_dataloader.pkl")
-        
+        train_dataloader = os.path.join(
+            config()["path"]["PROCESSED_PATH"], "train_dataloader.pkl"
+        )
+        valid_dataloader = os.path.join(
+            config()["path"]["PROCESSED_PATH"], "valid_dataloader.pkl"
+        )
+        test_dataloader = os.path.join(
+            config()["path"]["PROCESSED_PATH"], "test_dataloader.pkl"
+        )
+
         return {
-           "train_dataloader": train_dataloader,
-           "valid_dataloader": valid_dataloader,
-           "test_dataloader": test_dataloader, 
+            "train_dataloader": load(filename=train_dataloader),
+            "valid_dataloader": load(filename=valid_dataloader),
+            "test_dataloader": load(filename=test_dataloader),
         }
-        
+
     else:
-        raise FileNotFoundError("dataloader cannot be imported from the helper method".capitalize())
+        raise FileNotFoundError(
+            "dataloader cannot be imported from the helper method".capitalize()
+        )
+
 
 def helper(**kwargs):
     model = kwargs["model"]
@@ -42,7 +50,7 @@ def helper(**kwargs):
     smooth = kwargs["smooth"]
     alpha = kwargs["alpha"]
     gamma = kwargs["gamma"]
-    
+
     if model is None:
         model = attentionCNN(
             image_channels=config()["attentionCNN"]["image_channels"],
@@ -53,19 +61,19 @@ def helper(**kwargs):
             activation=config()["attentionCNN"]["activation"],
             bias=True,
         )
-        
+
     if adam:
-        optimizer = optim.Adam(params=model.parameters(), lr = lr, betas=(beta1, beta2)) 
+        optimizer = optim.Adam(params=model.parameters(), lr=lr, betas=(beta1, beta2))
     elif SGD:
         optimizer = optim.SGD(params=model.parameters(), lr=lr, momentum=momentum)
-        
+
     try:
         dataloader = load_dataloader()
     except FileNotFoundError as e:
         print("An error is occurred in the file ", e)
     except Exception as e:
         print("An error is occurred in the file ", e)
-        
+
     if loss == "dice":
         loss = DiceLoss(smooth=smooth)
     elif loss == "focal":
@@ -74,20 +82,55 @@ def helper(**kwargs):
         loss = IoULoss(smooth=smooth)
     elif loss == "tversky":
         loss = TverskyLoss()
-    elif loss == "combo":
-        loss = ComboLoss(alpha=alpha, gamma=gamma, smooth=smooth)
     else:
         loss = BinaryCrossEntropyLoss(reduction="mean")
-        
+
     return {
         "train_dataloader": dataloader["train_dataloader"],
         "valid_dataloader": dataloader["valid_dataloader"],
         "test_dataloader": dataloader["test_dataloader"],
         "model": model,
         "optimizer": optimizer,
-        "loss": loss
+        "loss": loss,
     }
 
+
 if __name__ == "__main__":
-    pass        
-    
+    init = helper(
+        model=None,
+        lr=0.001,
+        momentum=0.9,
+        loss="dice",
+        alpha=0.25,
+        gamma=2,
+        smooth=1e-4,
+        beta1=0.5,
+        beta2=0.999,
+        adam=True,
+        SGD=False,
+    )
+
+    assert (
+        init["train_dataloader"].__class__
+    ) == torch.utils.data.dataloader.DataLoader, (
+        "train_dataloader is not a dataloader".capitalize()
+    )
+    assert (
+        init["valid_dataloader"].__class__
+    ) == torch.utils.data.dataloader.DataLoader, (
+        "valid_dataloader is not a dataloader".capitalize()
+    )
+    assert (
+        init["test_dataloader"].__class__
+    ) == torch.utils.data.dataloader.DataLoader, (
+        "test_dataloader is not a dataloader".capitalize()
+    )
+    assert (
+        init["model"].__class__
+    ) == attentionCNN, "model is not a model".capitalize()
+    assert (
+        init["optimizer"].__class__
+    ) == optim.Adam, "optimizer is not an optimizer".capitalize()
+    assert (
+        init["loss"].__class__
+    ) == DiceLoss, "loss is not a loss function".capitalize()
