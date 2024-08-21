ff61f825c7c295744889a836f369d1830b55b000
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-21 19:44:09 +0200

    setup file is updated

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="attentionCNN",
+    version="0.0.1",
+    description="A deep learning project for Transformer-based image segmentation tasks using attentionCNN",
+    author="Atikul Islam Sajib",
+    author_email="atikulislamsajib137@gmail.com",
+    url="https://github.com/atikul-islam-sajib/attentionCNN",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="attentionCNN, machine-learning",
+    project_urls={
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/attentionCNN/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/attentionCNN",
+        "Source Code": "https://github.com/atikul-islam-sajib/attentionCNN",
+    },
+)
